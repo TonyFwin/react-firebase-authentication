57c27a2a86b5319e7edf8474cb271163ad07735a
Tony Nguyen <tonynguyen201@gmail.com>
2019-12-11 12:47:42 -0600

    add placeholder data for styling

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,63 @@
 import React from 'react';
 
-const Landing = () => (
-  <div>
-    <h1>Landing</h1>
-  </div>
-);
+function randomNum(num) {
+  return Math.floor(Math.random() * num);
+}
+
+const list = [
+  {
+    name: 'Tony Nguyen',
+    pod: 'Web',
+    title: 'Web Developer',
+    imageUrl: 'https://randomuser.me/api/portraits/men/30.jpg'
+  },
+  {
+    name: 'Tiffany Mutchler',
+    pod: 'Valkyrie',
+    title: 'Account Executive',
+    imageUrl: 'https://randomuser.me/api/portraits/women/34.jpg'
+  },
+  {
+    name: 'Jay Bachmayer',
+    pod: 'Enclave',
+    title: 'Growth Strategist',
+    imageUrl: 'https://randomuser.me/api/portraits/men/51.jpg'
+  },
+  {
+    name: 'Katie Levitt',
+    pod: 'Pickle',
+    title: 'Creative Director',
+    imageUrl: 'https://randomuser.me/api/portraits/women/3.jpg'
+  }
+];
+
+class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Landing</h1>
+        {this.state.list.map((item, index) => (
+          <div style={{ display: 'flex', alignItems: 'center' }} key={index}>
+            <div>
+              <img src={item.imageUrl} alt={item.title} />
+            </div>
+            <span>
+              <a href='#'>{item.name}</a>
+            </span>
+            <span>{item.title}</span>
+            <span>{item.pod}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Landing;
