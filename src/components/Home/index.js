8021c25f0f62a420612b64483a80660d8092d83e
Tony Nguyen <tonynguyen201@gmail.com>
2019-12-12 09:37:57 -0600

    add route protection for home and account routes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Home = () => (
+import { withAuthorization } from '../Session';
+
+const HomePage = () => (
   <div>
     <h1>Home</h1>
+    <p>The Home Page is accessible only by signed in users.</p>
   </div>
 );
 
-export default Home;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(HomePage);
